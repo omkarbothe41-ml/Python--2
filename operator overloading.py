694394baab0d957f0ecf5f6a9cae40ba0001788b
omkarbothe41-ml <omkarbothe41@gmail.com>
2026-03-10 11:26:01 +0530

    Create operator overloading.py

---
+++ operator overloading.py
@@ -0,0 +1,10 @@
+class box:
+    def __init__(self,weight):
+        self.weight=weight
+
+    def __add__(self,other):
+        return self.weight+ other.weight
+  
+b1=box(10)
+b2=box(20)
+print(b1+b2)
